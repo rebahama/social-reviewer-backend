1766faa353058018f43b302e11638a8b0d409a93
rebahama <wiri@hotmail.se>
2022-09-05 03:48:35 +0000

    add authorization for crud operations

--- profiles/serializer.py
+++ profiles/serializer.py
@@ -3,15 +3,16 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """ serialize the data to json"""
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
+        """ How to check for profile ownership"""
         request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = '__all__'
-
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from social_drf.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializer import ProfileSerializer
 
 
+
 class ProfileList(generics.ListAPIView):
+    """Retrive the profiles and set the """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.AllowAny]
 
 
 class ProfileListDetail(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsOwnerOrReadOnly]
