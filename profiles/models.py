317859383a2fcd7313e9835797fdca95b35ebd0a
rebahama <wiri@hotmail.se>
2022-09-05 01:50:37 +0000

    add admin settings for profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
@@ -21,3 +22,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.owner}'s profile"
 
+
+def create_profile(sender, instance, created, **kwargs):
+    """Every time a user is created
+       then a profile is
+       also created automatically"""
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
--- social_drf/settings.py
+++ social_drf/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fw)gkwrhci-4y-a4*aq#s56-f@cgjnb@_v^8z5m&hmn0%sn3zx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
 
-    'profiles'
+    'profiles',
 ]
 
 MIDDLEWARE = [
