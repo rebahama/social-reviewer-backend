8d435c556f6a1ed52b1aefaa5f3f90d43988a791
rebahama <wiri@hotmail.se>
2022-09-06 01:19:52 +0000

    add migration and create the post view

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from social_drf.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializer import PostSerializer
@@ -8,5 +8,7 @@ from .serializer import PostSerializer
 class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
