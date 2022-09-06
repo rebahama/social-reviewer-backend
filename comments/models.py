e890941d4f2960717511dff735db575ee149b131
rebahama <wiri@hotmail.se>
2022-09-06 03:06:36 +0000

    add migrations for comment model

--- comments/models.py
+++ comments/models.py
@@ -4,16 +4,17 @@ from posts.models import Post
 
 
 class Comments(models.Model):
-    RATING = ((1, 1,
-               2, 2,
-               3, 3,
-               4, 4,
-               5, 5,))
+    RATING = ((1, 'Very bad',),
+              (2, 'bad'),
+              (3, 'good'),
+              (4, 'Very good'),
+              (1, 'Extremely good'),
+    )
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(blank=True,
-             default=1, choices=RATING)
+             default=3, choices=RATING)
     content = models.TextField(max_length=1500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -23,5 +24,3 @@ class Comments(models.Model):
 
     def __str__(self):
         return self.content + ' ' + str(self.owner)
-    
-  
