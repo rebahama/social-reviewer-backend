345d08f6aa3e048073031f994e11e3b0aa3a45e3
rebahama <wiri@hotmail.se>
2022-09-12 01:51:18 +0000

    add enviorment variable in settings.py for later use

--- social_drf/settings.py
+++ social_drf/settings.py
@@ -65,14 +65,17 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV_CHECK' in os.environ
 
-ALLOWED_HOSTS = ['social-reviewer-backend.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost']
 
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN', ''), re.IGNORECASE).group(0)
+    extracted_url = re.match(r'^.+-',
+                             os.environ.get('CLIENT_ORIGIN_DEV', ''),
+                             re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
